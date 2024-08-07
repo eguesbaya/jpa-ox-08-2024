ece6c1da67975b3d738662b6477630d276971af2
Jeremy <jeremy.perrouault@ascadis.fr>
2024-08-07 11:47:18 +0200

    JOUR 1 - PREMIERE ENTITE

--- formation-jpa/src/main/java/fr/formation/Application.java
+++ formation-jpa/src/main/java/fr/formation/Application.java
@@ -1,7 +1,14 @@
 package fr.formation;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("FormationUnit");
+        EntityManager em = emf.createEntityManager();
+
+        
     }
 }
---
+++ formation-jpa/src/main/java/fr/formation/model/Produit.java
@@ -0,0 +1,23 @@
+package fr.formation.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "produit")
+@Getter @Setter
+@NoArgsConstructor
+public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String name;
+}
