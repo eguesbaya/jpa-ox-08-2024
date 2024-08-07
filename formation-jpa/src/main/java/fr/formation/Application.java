50d22068a1e6b24d97f9d0e4a325b3c851d00a85
Jeremy <jeremy.perrouault@ascadis.fr>
2024-08-07 14:17:34 +0200

    JOUR 1 - LISTE PRODUITS

--- formation-jpa/src/main/java/fr/formation/Application.java
+++ formation-jpa/src/main/java/fr/formation/Application.java
@@ -1,5 +1,8 @@
 package fr.formation;
 
+import java.util.List;
+
+import fr.formation.model.Produit;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -9,6 +12,12 @@ public class Application {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("FormationUnit");
         EntityManager em = emf.createEntityManager();
 
-        
+
+        List<Produit> produits = em.createQuery("select p from Produit p", Produit.class)
+            .getResultList();
+
+        System.out.println("J'ai " + produits.size() + " produits en base !");
+
+        emf.close();
     }
 }
--- formation-jpa/src/main/java/fr/formation/model/Produit.java
+++ formation-jpa/src/main/java/fr/formation/model/Produit.java
@@ -1,7 +1,12 @@
 package fr.formation.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,5 +24,23 @@ public class Produit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     
+    // @Column(name = "pro_name", columnDefinition = "VARCHAR(100) NOT NULL")
+    @Column(name = "pro_name", length = 100, nullable = false)
     private String name;
+
+    // @Transient
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "pro_type")
+    private Type type;
+
+    // @Temporal
+    // private Date date;
+
+    private LocalDate date;
+
+    public enum Type {
+        BUSINESS, CUSTOMER;
+    }
 }
