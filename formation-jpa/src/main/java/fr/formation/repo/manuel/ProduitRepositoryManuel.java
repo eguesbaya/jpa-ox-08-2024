45a6dc853b3aa5d803a949075397bed8a3003f8b
Jeremy <jeremy.perrouault@ascadis.fr>
2024-08-08 16:51:18 +0200

    JOUR 2 - CRITERIA QUERY EXO 2 (variante)

--- formation-jpa/src/main/java/fr/formation/repo/manuel/ProduitRepositoryManuel.java
+++ formation-jpa/src/main/java/fr/formation/repo/manuel/ProduitRepositoryManuel.java
@@ -4,10 +4,13 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+import fr.formation.model.Fournisseur;
 import fr.formation.model.Produit;
 import fr.formation.repo.ProduitRepository;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 
@@ -100,13 +103,31 @@ public class ProduitRepositoryManuel extends AbstractRepositoryManuel implements
         return em.createQuery(cq).getResultList();
     }
 
+    // @Override
+    // public List<Produit> findAllByFournisseurId(int id) {
+    //     CriteriaBuilder cb = em.getCriteriaBuilder();
+    //     CriteriaQuery<Produit> cq = cb.createQuery(Produit.class);
+    //     Root<Produit> root = cq.from(Produit.class);
+
+    //     Predicate idEquals = cb.equal(root.get("fournisseur").get("id"), id);
+
+    //     cq  .select(root)
+    //         .where(idEquals)
+    //     ;
+
+    //     return em.createQuery(cq).getResultList();
+    // }
+    
     @Override
     public List<Produit> findAllByFournisseurId(int id) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Produit> cq = cb.createQuery(Produit.class);
         Root<Produit> root = cq.from(Produit.class);
 
-        Predicate idEquals = cb.equal(root.get("fournisseur").get("id"), id);
+        // root.fetch("fournisseur", JoinType.LEFT);
+        Join<Produit, Fournisseur> joinFournisseur = root.join("fournisseur", JoinType.LEFT);
+
+        Predicate idEquals = cb.equal(joinFournisseur.get("id"), id);
 
         cq  .select(root)
             .where(idEquals)
