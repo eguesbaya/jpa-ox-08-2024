c032c8e1dff783260b341f0e3eb6a883ff0a40e7
Jeremy <jeremy.perrouault@ascadis.fr>
2024-08-09 10:17:01 +0200

    JOUR 3 - BOOT REPO DEMO

--- formation-jpa-boot/src/main/java/fr/formation/Application.java
+++ formation-jpa-boot/src/main/java/fr/formation/Application.java
@@ -3,7 +3,9 @@ package fr.formation;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import fr.formation.model.Fournisseur;
 import fr.formation.model.Produit;
+import fr.formation.repo.FournisseurRepository;
 import fr.formation.repo.ProduitRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,11 +18,16 @@ public class Application implements CommandLineRunner {
     // private ProduitRepository produitRepository;
 
     private final ProduitRepository produitRepository;
+    private final FournisseurRepository fournisseurRepository;
 
     @Override
     public void run(String... args) throws Exception {
         for (Produit produit : this.produitRepository.findAll()) {
             log.debug("Le produit {} (id = {})", produit.getName(), produit.getId());
+        }
+
+        for (Fournisseur fournisseur : this.fournisseurRepository.findAll()) {
+            log.debug("Le fournisseur = {}", fournisseur.getName());
         }
     }
 }
--- formation-jpa-boot/src/main/java/fr/formation/repo/FournisseurRepository.java
+++ formation-jpa-boot/src/main/java/fr/formation/repo/FournisseurRepository.java
@@ -2,8 +2,19 @@ package fr.formation.repo;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import fr.formation.model.Fournisseur;
 
-public interface FournisseurRepository extends Repository<Fournisseur, Integer> {
-    public List<Fournisseur> findAllByProduitCommentaireNote(int note);
+public interface FournisseurRepository extends JpaRepository<Fournisseur, Integer> {
+    public List<Fournisseur> findAllByProduitsCommentairesNote(int note);
+
+    public List<Fournisseur> findByIdInAndNameIsNull(List<Integer> ids);
+
+    @Query("select f from Fournisseur f where f.name = ?1")
+    public Fournisseur findByNom(String nom);
+
+    @Query(value = "select * from fournisseur where name = ?1", nativeQuery = true)
+    public Fournisseur findByNomNative(String nom);
 }
